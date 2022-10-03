981703f583b55f6431c7d8d1f9b0a9e68d194c78
cdmitchener <87343179+cdmitchener@users.noreply.github.com>
2022-10-03 17:02:14 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b6vgios1d*$m3q+j^&7dn8=w^ght_7znldjkxe=jfow_#@7xya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+#     'django.contrib.sites',
     'blog',
 ]
 
